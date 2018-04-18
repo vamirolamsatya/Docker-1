233e9698247c73ff3b9ddd112c26b93193758c01
BloodyNora <no@ema.il>
2018-04-18 23:43:16 +0200

    changed parameter

--- dockerfiles/bitcoin-scrypt/run.sh
+++ dockerfiles/bitcoin-scrypt/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --rm --network=host --hostname=bitcoin-scrypt --mount source=bitcoin-scrypt,target=/home/bitcoin-scrypt --cpus="2.0" -it bitcoin-scrypt
+${DOCKER} run --rm --network=host --hostname=bitcoin-scrypt --mount source=bitcoin-scrypt,target=/home/bitcoin-scrypt --cpus="2.0" -it bitcoin-scrypt /bin/bash
