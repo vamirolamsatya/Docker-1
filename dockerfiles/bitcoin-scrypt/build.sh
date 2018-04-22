8aebfcf43f1a69aac4da915a042be10c2080b2f7
BloodyNora <no@ema.il>
2018-04-22 13:41:47 +0200

    Refactored shell scripts into docker.sh

--- dockerfiles/bitcoin-scrypt/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GIT=$(which git)
-DOCKER=$(which docker)
-
-${GIT} pull .
-${DOCKER} build -t bitcoin-scrypt .
--- dockerfiles/bitcoin-scrypt/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DOCKER=$(which docker)
-${DOCKER} run --rm --network=host --hostname=bitcoin-scrypt --mount source=bitcoin-scrypt,target=/home/bitcoin-scrypt -it bitcoin-scrypt ${@}
