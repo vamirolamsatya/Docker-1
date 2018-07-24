a2db2811a20e8a37efe3c5c3b0c3ee9c124ebe78
Florian Geiges <flo@macbook-flo.local>
2018-07-24 11:54:29 +0200

    Gave container the name 'magiminer'.

--- wolf-m7m-cpuminer-v2/run.sh
+++ wolf-m7m-cpuminer-v2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --rm -it wolf-m7m-cpuminer-v2 -o stratum+tcp://xmg.minerclaim.net:7008 -u 0x03.x220 -p x220
+${DOCKER} run --rm -it wolf-m7m-cpuminer-v2 --name magiminer -o stratum+tcp://xmg.minerclaim.net:7008 -u 0x03.pro -p pro
