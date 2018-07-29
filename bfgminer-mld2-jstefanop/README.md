6ed399a778575e232523c5eeb405e6ffa5b8e01c
BloodyNora <0x03-ctrlc@protonmail.com>
2018-07-29 10:11:00 +0000

    introduced new run.sh and config files

--- bfgminer-mld2-jstefanop/README.md
+++ bfgminer-mld2-jstefanop/README.md
@@ -1,10 +1,13 @@
 # bfgminer-mld2-jstefanop
 
-This also works on a RaspberryPi 3.
+	- https://bitcointalk.org/index.php?topic=2420357.0
+	- http://github.com/jstefanop/bfgminer
+
+This also works on a RaspberryPi 3. In fact, thats what it was made for.
 
 ## Prerequisites
 
-Docker, ~1GB of disk space, Dockerfile.
+Docker, ~512MB of disk space, Dockerfile.
 
 
 ## Building the Docker image
@@ -15,6 +18,19 @@ Easiest way to get up and running:
 git clone https://github.com/BloodyNora/Docker
 cd Docker/bfgminer-mld2-jstefanop
 ./build.sh
-<edit run.sh to suit your needs>
-./run.sh
+# in the ./conf/ subdir, find a config file to copy and adapt.
+# then, assuming your config file is named yourpool.conf, run:
+./run.sh yourpool
+```
+
+## Did this help you? 
+
+Consider donating!
+
+```
+LTC:  MHJN9mHjgYzqHQPXNCeYuxmnN4N3vneSvR
+BTCS: 12aYzfPWEaYPiAwaVCPgM3DQgekXDFSrKv
+EMC2: EfN8Q6bado86srxa66nVKJkfcwDBhbiiow
+ABY:  ALHkvwbujjQCjFPujomuLdYi5ZbMeNGG7f
+DGB:  DFkcHMPgT9hR2VjFTx6QaSfSiazKhubnXD
 ```
--- bfgminer-mld2-jstefanop/run.sh
+++ bfgminer-mld2-jstefanop/run.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --name bfgminer-mld2 --privileged -v /dev//bus/usb:/dev/bus/usb --rm -itd bfgminer-mld2-jstefanop --scrypt -o stratum+tcp://litecoinpool.org:3333 -u BloodyNora.1 -p 1 -S ALL --set MLD:clock=876
+if [ -z "${DOCKER}" ]; then
+	echo "ERROR: Docker was not found. exiting!"
+	echo "to install, simply run: \"curl https://get.docker.com | sudo sh\""
+	exit
+fi
+
+if [ -z "${1}" ]; then
+	echo "ERROR: no config file given, exiting!"
+	echo "USAGE: ${0} example"
+	exit
+elif [ ! -f ./conf/${1}.conf ]; then
+	echo "ERROR: no such config file \"${1}.conf\", exiting!"
+	exit
+else
+	# load config
+	. ./conf/${1}.conf
+	# WORKER_PASS not needed on all pools, thus not checking
+	if [ -z "${POOL_HOST}" ] || [ -z "${WORKER_NAME}" ]; then
+		echo "ERROR: config file \"${1}.conf\" is either missing some crucial parameters or is complete crap. exiting!"
+		exit
+	else
+		if [ -z "${MOONLANDER_DEVICES}" ]; then
+			# use all available devices per default
+			MOONLANDER_DEVICES="ALL"
+		fi
+		if [ -z "${MOONLANDER_CLOCK}" ]; then
+			# per default a lower, pretty much safe clock speed
+			MOONLANDER_CLOCK="768"
+		fi
+
+		#
+		# docker startting happens here
+		# 
+		${DOCKER} run --name ${DOCKER_NAME} \
+			--privileged -v /dev/bus/usb:/dev/bus/usb --rm \
+			-it ${DOCKER_DETACH} bfgminer-mld2-jstefanop -o ${POOL_HOST} -u ${WORKER_NAME} -p "${WORKER_PASS}" -S ${MOONLANDER_DEVICES} --set MLD:clock=${MOONLANDER_CLOCK} ${BFGMINER_EXTRA_ARGS}
+	fi
+fi
+
+# EOF
