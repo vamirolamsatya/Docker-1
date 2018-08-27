804314b87a0e8665c99b005143f391c8e746915a
Bloody Nora <0x03-ctrlc@protonmail.com>
2018-08-27 20:20:38 +0200

    changed default pool ip to a hostname

--- nheqminer-veruscoin/run.sh
+++ nheqminer-veruscoin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --name nheqminer-veruscoin --rm -it nheqminer-veruscoin -v -l 95.216.13.66:9999 -u RB4E24growBcS5jMLrLTZJTToFdQ7r51VR -t 4
+${DOCKER} run --name nheqminer-veruscoin --rm -it nheqminer-veruscoin -v -l stratum.veruspool.xyz:9999 -u RB4E24growBcS5jMLrLTZJTToFdQ7r51VR -t 4
