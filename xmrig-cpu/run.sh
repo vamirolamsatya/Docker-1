80f528244bf0a1acc183e34c2c6491d174a24bf3
0x03 <4005813+BloodyNora@users.noreply.github.com>
2019-02-06 11:19:02 +0000

    Update run.sh

--- xmrig-cpu/run.sh
+++ xmrig-cpu/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --name xmrig-cpu --rm -it xmrig-cpu -o stratum+tcp://prohashing.com:3341 -u ctrlc -p "e=off n=donation"
+${DOCKER} run --name xmrig-cpu --rm -it xmrig-cpu --donation-level=1 -o stratum+tcp://prohashing.com:3341 -u ctrlc -p "e=off n=donation"
