30642523953883fcbf00f2b67d195a010073d86e
0x03 <4005813+BloodyNora@users.noreply.github.com>
2019-02-06 11:38:18 +0100

    Update run.sh

--- wolf-m7m-cpuminer-v2/run.sh
+++ wolf-m7m-cpuminer-v2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DOCKER=$(which docker)
-${DOCKER} run --name magiminer --rm -it wolf-m7m-cpuminer-v2 -o stratum+tcp://xmg.minerclaim.net:7008 -u 0x03.pro -p pro
+${DOCKER} run --name magiminer --rm -it wolf-m7m-cpuminer-v2 -o stratum+tcp://xmg.minerclaim.net:7008 -u 0x03.donation -p donation
