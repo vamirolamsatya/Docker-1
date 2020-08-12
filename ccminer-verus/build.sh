3e42a74c63296955db3f4da3767291d7c59d0f9e
0x03 <0x03-ctrlc@protonmail.com>
2020-08-12 17:02:57 +0200

    added error in case git isn't found

--- ccminer-verus/build.sh
+++ ccminer-verus/build.sh
@@ -6,6 +6,9 @@ DOCKER=$(which docker)
 if [ -z "${DOCKER}" ]; then
     echo "ERROR: Docker does not seem to be installed. Please download and install Docker CE as outlined on https://docs.docker.com/engine/install/."
     exit 1
+elif [ -z "${GIT}" ]; then
+	echo "ERROR: git does not seem to be installed. Please install git for your OS/distribution."
+	exit 1
 else
 	${GIT} pull
 	${DOCKER} build -t ${IMAGE_NAME} .
